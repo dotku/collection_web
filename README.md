b8feb96fbe46a5469ef14d2c5c14f6f87b273a1a
Jay Lin <weijingjaylin@gmail.com>
2016-02-03 21:09:02 -0800

    work commit, update this project to work dir

---
+++ README.md
@@ -0,0 +1,9 @@
+# WebSite
+Live: http://www.weijinglin.name/website
+## blog 
+- http://www.techslides.com 94,813 Indian  
+
+## JavaScript framework
+- https://www.polymer-project.org
+- http://www.gwtproject.org, from Google
+- http://www.javelinjs.com/, from Facebook
---
+++ app/Common/Conf/config.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	// MySQL
+    'DB_TYPE'      =>  'mysql',     // 数据库类型  
+    'DB_HOST'     =>  'localhost',     // 服务器地址  
+    'DB_NAME'    =>  'weijinglin_general',     // 数据库名  
+    'DB_USER'      =>  'root',     // 用户名  
+    'DB_PWD'       =>  'admin123',     // 输入安装MySQL时设置的密码  
+    'DB_PORT'      =>  '3306',     // 端口  
+    'DB_PREFIX'    =>  'wjl_',     // 数据库表前缀  
+    'DB_DSN'       =>  '',     // 数据库连接DSN 用于PDO方式  
+	// ThinkPHP 配置
+	'TMPL_ENGINE_TYPE' =>'PHP'
+);
